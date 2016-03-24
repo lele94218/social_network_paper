18d1b069bfc9bf141f61d5fafe5145a6918e0dd2
Taoran Xue <terry.trxue@gmail.com>
2016-03-24 10:43:20 +0800

    完善 Twitter 爬虫

--- get_twitter_timeline/TestSocks.py
+++ get_twitter_timeline/TestSocks.py
@@ -1,6 +1,16 @@
-import socks
-import socket
-socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 1080)
-socket.socket = socks.socksocket
-import urllib2
-print urllib2.urlopen('http://www.google.com').read()
+# import socks
+# import socket
+# socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 1080)
+# socket.socket = socks.socksocket
+# import urllib2
+# print urllib2.urlopen('http://www.google.com').read()
+
+import traceback
+
+try:
+    1 / 1
+except Exception, e:
+    exstr = traceback.format_exc()
+    print exstr
+finally:
+    print 'ok'
--- get_twitter_timeline/TwitterApi.py
+++ get_twitter_timeline/TwitterApi.py
@@ -20,7 +20,7 @@ api = twitter.Api(consumer_key=c_key,
 
 def save_user(tw_user_id=None, name=None, screen_name=None, location=None, language=None,
               description=None, profile_image_url=None, rcmd_user_ids=None, since_id=None, max_id=None):
-    db = MySQLdb.connect(user="root", passwd="1234", db="firends_recommend", host="localhost",
+    db = MySQLdb.connect(user="root", passwd="133433", db="firends_recommend", host="localhost",
                          cursorclass=DictCursor)
     cursor = db.cursor()
     print tw_user_id
@@ -36,43 +36,138 @@ def save_user(tw_user_id=None, name=None, screen_name=None, location=None, langu
         exstr = traceback.format_exc()
         print exstr
         db.rollback()
-
-    db.close
+    finally:
+        db.close
 
 
 def get_users_show():
     users = api.GetFriends()
 
     for user in users:
+        # user = users[0]
         save_user(tw_user_id=user.id, name=user.name.encode('utf8'),
                   screen_name=user.screen_name.encode('utf8'), location=user.location.encode('utf8'),
                   description=user.description.encode('utf8'),
                   profile_image_url=user.profile_image_url)
 
+    for user in users:
+        fetch_tweets(tw_user_id=user.id)
 
-def save_tweet(tw_tweet_id=None, text=None, user_id=None, created_at=None, lang=None, coordinates=None):
-    db = MySQLdb.connect(user="root", passwd="19940218aA", db="firends_recommend", host="localhost",
+
+def save_tweet(tw_tweet_id=None, text=None, user_id=None, created_at=None, lang=None, coordinates_x=None,
+               coordinates_y=None):
+    db = MySQLdb.connect(user="root", passwd="133433", db="firends_recommend", host="localhost",
                          cursorclass=DictCursor)
     cursor = db.cursor()
     print tw_tweet_id
     try:
         cursor.execute(
-            """INSERT INTO Tweet(tw_tweet_id, text, user_id, created_at, lang, coordinates)
-               VALUES (%s,%s,%s,%s,%s,%s)""",
-            (tw_tweet_id, text, user_id, created_at, lang, coordinates))
+            """INSERT INTO Tweet(tw_tweet_id, text, user_id, created_at, lang, coordinates_x, coordinates_y)
+               VALUES (%s,%s,%s,%s,%s,%s,%s)""",
+            (tw_tweet_id, text, user_id, created_at, lang, coordinates_x, coordinates_y))
         db.commit()
     except Exception, e:
         exstr = traceback.format_exc()
         print exstr
         db.rollback()
+    finally:
+        db.close
 
+
+def get_user(tw_user_id):
+    db = MySQLdb.connect(user="root", passwd="133433", db="firends_recommend", host="localhost",
+                         cursorclass=DictCursor)
+    cursor = db.cursor()
+    try:
+        cursor.execute(
+            " SELECT * FROM User WHERE tw_user_id = %s " % (tw_user_id,))
+        user = cursor.fetchone()
+        return user
+    except Exception, e:
+        exstr = traceback.format_exc()
+        print exstr
+        db.rollback()
+    finally:
+        db.close
+
+
+def save_user_since_id(since_id, user_id):
+    db = MySQLdb.connect(user="root", passwd="133433", db="firends_recommend", host="localhost",
+                         cursorclass=DictCursor)
+    cursor = db.cursor()
+    try:
+        cursor.execute(
+            " UPDATE User SET since_id = %s WHERE user_id = %s " % (since_id, user_id))
+        db.commit()
+    except Exception, e:
+        exstr = traceback.format_exc()
+        print exstr
+        db.rollback()
+    finally:
+        db.close
+
+
+def fetch_tweets(tw_user_id):
+    _tw_user_id = tw_user_id
+    _user = get_user(_tw_user_id)
+
+    _since_id = '912455436390150100'
+
+    if _user['since_id'] is not None:
+        _since_id = _user['since_id']
+
+    print _since_id
+
+    # 往前爬数据
+    status = api.GetUserTimeline(user_id=tw_user_id, count='200', max_id=_since_id)
+
+    try:
+        for statu in status:
+            _since_id = statu.id
+
+            date = statu.created_at
+            # Not fix %z instead of +0000 with Python 2.7
+            date = datetime.datetime.strptime(date, "%a %b %d %H:%M:%S +0000 %Y")
+            n_date = date.strftime('%Y-%m-%d %H:%M:%S')
+
+            _coordinates_x = None
+            _coordinates_y = None
+
+            if statu.coordinates is not None:
+                _coordinates_x = statu.coordinates['coordinates'][0]
+                _coordinates_y = statu.coordinates['coordinates'][1]
+
+            save_tweet(tw_tweet_id=statu.id, text=statu.text.encode('utf8'), user_id=_user['user_id'],
+                       created_at=n_date, lang=_user['language'], coordinates_x=_coordinates_x,
+                       coordinates_y=_coordinates_y)
+    except Exception, e:
+        exstr = traceback.format_exc()
+        print exstr
+    finally:
+        print _since_id
+        save_user_since_id(since_id=_since_id, user_id=_user['user_id'])
+
+
+# get_users_show()
+
+db = MySQLdb.connect(user="root", passwd="133433", db="firends_recommend", host="localhost",
+                     cursorclass=DictCursor)
+cursor = db.cursor()
+try:
+    cursor.execute(
+        " SELECT * FROM User ")
+    t_users = cursor.fetchall()
+    for t_user in t_users:
+        if t_user['since_id'] is None:
+            print t_user['tw_user_id']
+            fetch_tweets(t_user['tw_user_id'])
+
+except Exception, e:
+    exstr = traceback.format_exc()
+    print exstr
+    db.rollback()
+finally:
     db.close
-
-
-status = api.GetUserTimeline(user_id='123780906')
-date = status[0].created_at
-# Not fix %z instead of +0000 with Python 2.7
-date = datetime.datetime.strptime(date, "%a %b %d %H:%M:%S +0000 %Y")
-n_date = date.strftime('%Y-%m-%d %H:%M:%S')
-
-save_tweet(tw_tweet_id=12345, user_id=24, created_at=n_date)
+#
+# stat = api.GetStatus(id='712821748031336448')
+# print stat.coordinates['coordinates'][0]
